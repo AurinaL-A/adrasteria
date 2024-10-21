fa81e73c89301b8dba5411e153d406f46b43b0dc
Aurina_LA <irina.shapochkina5432@gmail.com>
2024-10-21 18:51:43 +0500

    bannerblock

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import banner_img from '@/image/banner.png'
+import banner_img_bt from '@/image/banner_bottom.svg'
+import Banner from "@/components/banner/banner.module";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.container}>
-          
-        </div>
-      </main>
+      <Banner/>
     </div>
   );
 }
